9fdd4e7ac4e381a194bd83e1ad89f2648a6f6170
Zachariah Moreno <xachmoreno@gmail.com>
2012-02-20 23:07:34 -0800

    first commit

---
+++ all.js
@@ -0,0 +1,8 @@
+chrome.pageAction.setIcon(object details);
+
+chrome.pageAction.setPopup(object details);
+
+chrome.pageAction.setTitle(object details);
+
+chrome.pageAction.show(integer tabId);
+
---
+++ speech.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+      function startSearch(event) {
+        event.target.form.submit();
+      }
+</script>
